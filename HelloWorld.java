2d4271dde74e41e7bfe6f2c7d04916a69b9f07b0
Saketh Katta <sakethkatta04@gmail.com>
2026-02-16 22:38:41 -0500

    Add second print statement to HelloWorld
    Updated HelloWorld.java to include a message about GitHub version control. Recompiled the program, which updated the .class file. This commit demonstrates how local changes are tracked before being pushed to the repository.

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,7 @@ public class HelloWorld {
     public static void main(String[] args) {
     
     System.out.print("Hello World");
+    System.out.print("Program modified, but able to revert to original version using GitHub Desktop");
     
     }
         
